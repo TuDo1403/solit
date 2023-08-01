1eba352af9b0ac0dc8eeaf197eb2481dd3e1260a
tasiby <tuanhoang.uit@gmail.com>
2023-08-01 15:27:29 +0700

    update: revert fnSig

--- contracts/libraries/CustomRevert.sol
+++ contracts/libraries/CustomRevert.sol
@@ -18,4 +18,21 @@ library CustomRevert {
             revert Execution_Failed();
         }
     }
+
+    function _revertFnSig(
+        bytes memory returnData,
+        address target,
+        bytes4 fnSig,
+        function(address, bytes4) internal view customRevert
+    ) internal view {
+        if (returnData.length > 0) {
+            assembly {
+                let revertSize := mload(returnData)
+                revert(add(returnData, 0x20), revertSize)
+            }
+        } else {
+            customRevert(target, fnSig);
+            revert Execution_Failed();
+        }
+    }
 }
